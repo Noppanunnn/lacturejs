edf91823ad35a41784b68455059c86b71959c262
Noppanunnn <s6519n10002@sau.ac.th>
2025-02-25 12:33:53 +0700

    finish

---
+++ js/study03.js
@@ -0,0 +1,38 @@
+let data1 = "Hello....."; // Hello ...
+let data2 = 20; //Number: Integer เลขจำนวนเต็ม, Float เลขจำนวนที่มีทศนิยม
+let data3 = true; //Boolean: true/false
+// mosetelelee305
+//index number 0
+let data4 = [10, 20, 30, 40, 50]
+let data6 = ['John', 20, true]
+let data5 = {
+fullname: 'John',
+age: 20,
+status: true,
+color: ['red', 'green', 'blue'],
+address: {
+no: '10/5',
+soi: 'Soi 10',
+street: 'Petchakasem',
+province: 'Bangkok'
+}
+}
+
+
+let data8 = null
+let data9;
+
+console.log(data8)
+console.log(data9)
+console. log('-----------------')
+
+console. log(data5.fullname)
+console.log(data5.status)
+console.log(data5.color[1])
+console.log(data5.address.street)
+console. log('-----------------')
+
+console.log(data1)
+console.log(data2)
+console.log(data3)
+console. log('-----------------')
--- js/tset03-3-script.js
+++ js/tset03-3-script.js
@@ -1,2 +1,17 @@
-alert("Heyyyyyyyyyy")
-console.log("Heyyyyyyyyyy");
+//value of variable
+let data1 = 'Hello ... '; //"Hello ... "
+let data2 = 20;
+let data3 = true;
+//index number 0
+let data4 = [10, 20, 30, 40, 50]
+
+let data5 = {
+    fullname: 'John',
+    age: 20,
+    color: ['red', 'green', 'blue']
+}
+
+
+console.log(data1)
+console.log(data2)
+console.log(data3)
