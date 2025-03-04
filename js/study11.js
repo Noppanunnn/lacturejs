a6dea771d515ec0c153a782eddce6054772e586b
SauSupawitTuysati <S6752D10029@sau.ac.th>
2025-03-04 09:42:56 +0700

    verfinal2

---
+++ js/study11.js
@@ -0,0 +1,18 @@
+// Exception Handling: Erorr/Excption
+//try-catch
+//try-catch-finaly
+
+try{
+    console.log(data1)
+}catch(err){
+    console.log('พบปัญหาการใฃ้งานติดต่อ Admin/IT....')
+    //console.log(err.name)
+    console.log(err.massge)
+    //console.log(err.stack)
+    
+
+}finally{
+    console.log('ขอบคุณที่ใช้บริการ')
+}
+
+
